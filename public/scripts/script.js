e2acce7adbd74cbffd6e2369db9f389e91137c2a
MazurkiewiczR <mazurkiewicz.er@gmail.com>
2024-02-13 16:31:38 +0100

    styled back to top btn, added metatags

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -104,7 +104,11 @@ document.addEventListener("scroll", () => {
   }
 });
 
-function scrollToTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+const scrollToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+
+scrollToTopBtn.addEventListener("click", () => {
+  scrollToTop();
+});
